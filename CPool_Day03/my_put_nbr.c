89016cf8220814514be3ef03b612f8c537310aa8
17150002 <17150002@bjtu.edu.cn>
2018-03-12 21:54:04 +0800

    second commit

---
+++ CPool_Day03/my_put_nbr.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void my_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int my_put_nbr(int nb)
+{
+	if(nb >= 10)
+	{
+		my_put_nbr(nb/10);
+		nb = nb%10;
+	}
+
+	if(nb >= 0 && nb < 10)
+	{
+		char c = nb + '0'; 
+		my_putchar(c);
+	}
+
+	if(nb<0)
+	{
+		int n;
+		n = -1 * nb;
+		my_putchar('-');
+		if(n >= 10)
+		{
+			my_put_nbr(n/10);
+			n = n%10;
+		}
+
+		if(n >= 0 && n < 10)
+		{
+			char c = n + '0'; 
+			my_putchar(c);
+		}
+	}
+	return 0;
+}
